e9d7e7f93c45b46ba46043b63beb085d36ed46dd
shize.wang <shize.wang@shopee.com>
2022-11-26 22:19:45 +0800

    update

--- apue.3e/fileio/hole.c
+++ apue.3e/fileio/hole.c
@@ -4,10 +4,9 @@
 char	buf1[] = "abcdefghij";
 char	buf2[] = "ABCDEFGHIJ";
 
-int
-main(void)
+int main(void)
 {
-	int		fd;
+	int fd;
 
 	if ((fd = creat("file.hole", FILE_MODE)) < 0)
 		err_sys("creat error");
